29743a1de435ddebff7aed5f57a2247f39da96f1
peteryunkim <peteryunkim@gmail.com>
2017-07-23 14:12:56 -0400

    testing

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import AnswerForm from '../components/answerForm'
 
-const main = () => {
 
-	return(
-		<div>
-			Main
-		</div>
-	)
+class main extends React.Component {
+	constructor(){
+    super()
+    this.state = {
+			collection: ["Settlers of Catan", "7 Wonders", "Pandemic", "Dead Of Winter"]
+		}
+  }
+
+ 	handleSubmit = (game) => {
+ 		this.setState({
+ 			collection: [...this.state.collection, game]
+ 		})
+ 	}	 
+
+  render(){
+  	// debugger
+  	let list = this.state.collection.map(game => <li>{game}</li>)
+  	return(
+  		<div>
+	  		<ul>
+	  			{list}
+	  		</ul>
+	  		<h5> 
+	  			Add a game: <AnswerForm onSubmit={this.handleSubmit}/>
+	  		</h5>
+	  	</div>
+  	)
+  }
 }
 
 export default main
